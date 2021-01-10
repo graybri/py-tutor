cbc68a4dfdb38264f6cf3bfa809bc13857dba826
Brian Gray <61435864+graybri@users.noreply.github.com>
2021-01-10 17:20:02 -0500

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,10 @@
+# New comment
+# Another comment
+print('hello world')
+print('hello world')
+message = "'hello'"
+
+print(message.upper())
+
+
+print('testing')
---
+++ strings.py
@@ -0,0 +1,50 @@
+# New comment
+# Another comment
+import os
+
+print('Hello   World')
+message = 'Hello World'
+print(message)
+# Embedded Quote
+
+print(message)
+print("Bobby's World")
+
+
+print('Bobby\'s World')
+# multiline triple quotes '''
+message2 = '''Hello World was a
+good place to
+hang out'''
+print(message2)
+
+# characters in a string
+
+print(len(message))
+print(len(message2))
+
+# Access characters in string, index starting by 0 last  = -1
+# Sliceing strings
+print(message[0])
+print(message[-1])
+
+# print range of characters
+
+print(message[0:5])
+# Start at 0 end at 4 (5 not inclusive)
+
+print(message[:5])
+print(message[6:])
+
+# str methods
+print(message.lower())
+print(message.upper())
+
+# Count occurances of substring
+print(message.count('l'))
+print(message.count('Hello'))
+
+# find and replace substring
+print(message.replace('l', 't'))
+print(message.replace('World', 'Universet'))
+print(message.find('l'))
